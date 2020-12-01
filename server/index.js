98375064a0cf0255c103d050829e2a082c4c5a2a
Katie <adlerk8@gmail.com>
2020-12-01 16:49:21 -0700

    slowly but surely set up server

---
+++ server/index.js
@@ -0,0 +1,38 @@
+require('dotenv').config();
+const express = require('express');
+const massive = require('massive');
+const session = require('express-session');
+
+const ctrl = require('./controller');
+
+const {SESSION_SECRET, CONNECTION_STRING, SERVER_PORT} = process.env;
+
+const app = express();
+
+app.use(express.json());
+
+app.use(session({
+    resave: false,
+    saveUninitialized: true,
+    secret: SESSION_SECRET,
+    cookie: {maxAge: 1000 * 60 * 60 * 24 * 7}
+}))
+
+massive({
+    connectionString = CONNECTION_STRING,
+    ssl: {rejectUnauthorized: false}
+}).then((db) => {
+    app.set('db', db)
+    console.log("Database jacked up and good to go!")
+}).catch(err => console.log("Database error: " + err))
+
+// User Endpoints:
+app.post('/api/login', ctrl.login);
+app.get('/api/user', ctrl.getUser);
+app.put('/api/user', ctrl.editUser);
+app.post('/api/register', ctrl.register);
+app.delete('/api/logout', ctrl.logout);
+
+// Content Endpoints:
+
+app.listen(SERVER_PORT, () => console.log(`Server reporting for duty on port ${SERVER_PORT}, judicator`));
