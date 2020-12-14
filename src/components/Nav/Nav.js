9011182f1fd70079d6eb22343bbc0db0a2fa2974
Katie <adlerk8@gmail.com>
2020-12-14 00:06:49 -0700

    CSS for Nav in progress

--- src/components/Nav/Nav.js
+++ src/components/Nav/Nav.js
@@ -3,6 +3,32 @@ import { Link } from 'react-router-dom';
 import axios from 'axios';
 import { connect } from 'react-redux';
 import { logoutUser } from '../../redux/reducer';
+import styled from 'styled-components';
+
+const NavStyle = styled.div`
+    background-color: #011936;
+    height: 100%;
+    width: 200px;
+    position: fixed;
+    z-index: 1;
+    top: 170px;
+    left: 0;
+    overflow-x: hidden;
+    padding-top: 20px;
+    display: flex;
+`
+
+const LinkStyle = styled.div`
+    color: whitesmoke;
+    font-family: 'Hammersmith One', sans-serif;
+    font-size: 20px;
+    display: flex;
+    flex-direction: column;
+    justify-content: space-evenly;
+    &:visited {
+        color: whitesmoke;
+    }
+`    
 
 const Nav = (props) => {
     
@@ -12,12 +38,14 @@ const Nav = (props) => {
     };
     
     return (
-        <div>
-            <Link to="/newpost">Write Something</Link>
-            <Link to="/home">Home</Link>
-            <Link to="/myposts">My Posts</Link>
-            <Link to="/" onClick={logout}>Logout</Link>
-        </div>
+        <NavStyle>
+            <LinkStyle>
+                <Link to="/newpost">Write Something</Link>
+                <Link to="/home">Home</Link>
+                <Link to="/myposts">My Posts</Link>
+                <Link to="/" onClick={logout}>Logout</Link>
+            </LinkStyle>
+        </NavStyle>
     )
 };
 
